10cc946f423e1bf43931d51c476fce9997728def
Bonzani-Gab <bonzani.gabriele@iisgalvanimi.edu.it>
2026-04-24 09:08:40 +0000

    commenti

--- arrayy/src/main.ts
+++ arrayy/src/main.ts
@@ -1,6 +1,17 @@
+ // Importa la funzione che avvia (bootstrap) un'app Angular standalone nel browser
 import { bootstrapApplication } from '@angular/platform-browser';
+
+// Importa la configurazione globale dell'app (provider, routing, ecc.)
 import { appConfig } from './app/app.config';
+
+// Importa il componente principale dell'app (root component)
 import { App } from './app/app';
 
+
+// Avvia l'app Angular nel browser
+// - App → componente root
+// - appConfig → configurazioni globali dell'app
 bootstrapApplication(App, appConfig)
+
+  // Gestione degli errori nel caso il bootstrap fallisca
   .catch((err) => console.error(err));
